e750493f7bfd3e94fb816edec89ea050ef135ad8
SuhasKanwar <suhas.kanwar@gmail.com>
2025-02-16 15:48:56 +0530

    Fixed the socket issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+import { SocketProvider } from "./providers/Socket";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <SocketProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </SocketProvider>
     </div>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { useSocket } from "../providers/Socket";
 
 const Home = () => {
+  const socket = useSocket();
+  socket.emit("join-room", { roomID: "69", emailID: "suhas.kanwar@gmail.com" });
+  const handleJoinRoom = () => {
+    // handling code
+  };
   return (
     <div className="container">
-        <div>
-            <input type="email" placeholder="Enter your email" />
-            <input type="text" placeholder="Enter your room code" />
-            <button>Enter Room</button>
-        </div>
+      <div>
+        <input type="email" placeholder="Enter your email" />
+        <input type="text" placeholder="Enter your room code" />
+        <button>Enter Room</button>
+      </div>
     </div>
   );
 };
