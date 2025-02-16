2d3cd99d16f7cf310a832ef004ac6b6877a00264
SuhasKanwar <suhas.kanwar@gmail.com>
2025-02-16 15:53:21 +0530

    Handeled room join logic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSocket } from "../providers/Socket";
 
 const Home = () => {
   const socket = useSocket();
-  socket.emit("join-room", { roomID: "69", emailID: "suhas.kanwar@gmail.com" });
+
+  const [email, setEmail] = useState("");
+  const [roomID, setRoomID] = useState("");
+
   const handleJoinRoom = () => {
-    // handling code
+    socket.emit("join-room", { emailID: email, roomID });
   };
   return (
     <div className="container">
       <div>
-        <input type="email" placeholder="Enter your email" />
-        <input type="text" placeholder="Enter your room code" />
-        <button>Enter Room</button>
+        <input type="email" placeholder="Enter your email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="text" placeholder="Enter your room code" value={roomID} onChange={e => setRoomID(e.target.value)} />
+        <button onClick={handleJoinRoom}>Enter Room</button>
       </div>
     </div>
   );
