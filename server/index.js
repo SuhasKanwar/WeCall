9c10c1d37367bd90f0d4c899b2471976ab328826
SuhasKanwar <suhas.kanwar@gmail.com>
2025-02-16 15:43:05 +0530

    Setting up the providers and connection

--- server/index.js
+++ server/index.js
@@ -1,38 +1,40 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const { Server } = require('socket.io');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { Server } = require("socket.io");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const io = new Server();
+const io = new Server({
+  cors: true,
+});
 
 // Middlewares
-const { logger } = require('./middlewares/logger');
+const { logger } = require("./middlewares/logger");
 app.use(cors());
-app.use(logger('logs.txt'));
+app.use(logger("logs.txt"));
 app.use(bodyParser.json());
 
 const emailToScoketMapping = new Map();
 
-io.on('connection', (socket) => {
-    socket.on('join-room', data => {
-        const { roomID, emailID } = data;
-        console.log(`User with email ${emailID} joined room ${roomID}`);
-        emailToScoketMapping.set(emailID, socket.id);
-        socket.join(roomID);
-        socket.broadcast.to(roomID).emit('user-joined', { emailID });
-    });
+io.on("connection", (socket) => {
+  socket.on("join-room", (data) => {
+    const { roomID, emailID } = data;
+    console.log(`User with email ${emailID} joined room ${roomID}`);
+    emailToScoketMapping.set(emailID, socket.id);
+    socket.join(roomID);
+    socket.broadcast.to(roomID).emit("user-joined", { emailID });
+  });
 });
 
-app.get('/', (req, res) => {
-    return res.end("WeCall server has started successfully");
+app.get("/", (req, res) => {
+  return res.end("WeCall server has started successfully");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log(`\n\nhttp://localhost:${PORT}\n\n`);
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`\n\nhttp://localhost:${PORT}\n\n`);
 });
 
-io.listen(PORT + 1);
+io.listen(9001);
