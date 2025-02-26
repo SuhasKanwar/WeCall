daf7bb7b5ebb364313cdb6b7753f36967afa4eff
SuhasKanwar <suhas.kanwar@gmail.com>
2025-02-27 00:12:01 +0530

    Adding home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Lobby from "./pages/Lobby";
 import Room from "./pages/Room";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Lobby />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/lobby" element={<Lobby />} />
         <Route path="/room/:roomId" element={<Room />} />
       </Routes>
     </div>
