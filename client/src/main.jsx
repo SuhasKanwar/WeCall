08ddfd12bae32f3555966e95c0eb6e43faf9c9b8
SuhasKanwar <suhas.kanwar@gmail.com>
2025-03-02 19:27:54 +0530

    Added theme provider and navbar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { SocketProvider } from "./context/SocketProvider.jsx";
+import { ThemeProvider } from "./context/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <SocketProvider>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </SocketProvider>
   </BrowserRouter>
 );
