d72a5cfa284f661ab7817032687214184764f9e3
SuhasKanwar <suhas.kanwar@gmail.com>
2025-03-02 19:36:19 +0530

    Added mouse move effect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Lobby from "./pages/Lobby";
 import Room from "./pages/Room";
 import Home from "./pages/Home";
+import MouseMoveEffect from "./components/MouseMoveEffect";
 
 function App() {
   return (
     <div className="App">
+      <MouseMoveEffect />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/lobby" element={<Lobby />} />
