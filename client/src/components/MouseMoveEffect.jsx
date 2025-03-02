b0d17a089cc25ad67745df1402f5001e4f05bc77
SuhasKanwar <suhas.kanwar@gmail.com>
2025-03-02 19:43:23 +0530

    changed mouse effect for bright mode

--- client/src/components/MouseMoveEffect.jsx
+++ client/src/components/MouseMoveEffect.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
+import { useTheme } from "../context/ThemeProvider"
 
 export default function MouseMoveEffect() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+  const { theme } = useTheme()
 
   useEffect(() => {
     const handleMouseMove = (event) => {
@@ -13,13 +15,15 @@ export default function MouseMoveEffect() {
     return () => {
       window.removeEventListener("mousemove", handleMouseMove)
     }
-  }, [])
+  }, []);
 
   return (
     <div
       className="pointer-events-none fixed inset-0 z-30 transition-opacity duration-300"
       style={{
-        background: `radial-gradient(600px at ${mousePosition.x}px ${mousePosition.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
+        background: theme === "light"
+          ? `radial-gradient(600px at ${mousePosition.x}px ${mousePosition.y}px, rgb(233, 245, 216, 0.15), transparent 80%)`
+          : `radial-gradient(600px at ${mousePosition.x}px ${mousePosition.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`
       }}
     />
   )
