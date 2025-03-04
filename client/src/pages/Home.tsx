7a59b3734bb962a84fd99267f971c7e4409a7ece
SuhasKanwar <suhas.kanwar@gmail.com>
2025-03-05 01:34:06 +0530

    Added HERO section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import HeroSection from '../components/HeroSection'
 import NavBar from '../components/NavBar'
 
 export default function Home() {
   return (
     <div>
       <NavBar />
+      <HeroSection badge="WeCall" title1="Seamless Calls" title2=" Instant Connections" logoUrl="/logo.svg" />
     </div>
   )
 }
