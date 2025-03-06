f0e60820f062959e72c71c672216b94f1c1fd7e6
SuhasKanwar <suhas.kanwar@gmail.com>
2025-03-06 16:23:53 +0530

    Added timeline component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '../components/HeroSection'
 import NavBar from '../components/NavBar'
+import Timeline from '../components/TimeLine'
 
 export default function Home() {
   return (
     <div className='flex flex-col items-center'>
       <NavBar />
       <HeroSection badge="WeCall" title1="Seamless Calls" title2=" Instant Connections" logoUrl="/logo.svg" />
+      <Timeline />
     </div>
   )
 }
