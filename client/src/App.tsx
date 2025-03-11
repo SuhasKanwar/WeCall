ba329ecf5c520b22236a39184d8261828c571217
SuhasKanwar <suhas.kanwar@gmail.com>
2025-03-11 23:55:46 +0530

    Added scrillbar styling and custom mouse

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Lobby from "./pages/Lobby";
 import Room from "./pages/Room";
 import Home from "./pages/Home";
-import MouseMoveEffect from "./components/MouseMoveEffect";
+import MouseMoveEffect, { CustomCursor } from "./components/MouseMoveEffect";
 
 function App() {
   return (
     <div className="App">
+      <CustomCursor />
       <MouseMoveEffect />
       <Routes>
         <Route path="/" element={<Home />} />
